cdbae0ee8a1e0a70567b6ca5290b4f4af6f9eb32
aya.sayed1 <aya.sayed1@vodafone.com>
2024-03-28 17:12:33 +0200

    chore: refactor code

--- guessMyNumber/main.js
+++ guessMyNumber/main.js
@@ -1,5 +1,6 @@
-'use strict';
+"use strict";
 
+const secretNumber = document.getElementById("secretNumber");
 const message = document.getElementById("message");
 const scoreEle = document.getElementById("score");
 const highScore = document.getElementById("highScore");
@@ -7,35 +8,49 @@ let numberToGuess = getRandomInt();
 let score = 20;
 
 function getRandomInt() {
-    return Math.floor((Math.random() * 20) + 1);
+  return Math.floor(Math.random() * 20 + 1);
+}
+function isLosingGame() {
+  return score === 0;
 }
 
-function checkNumber(){
-    const number = parseInt(document.getElementById("GuessingNumber").value);
+function displayMessage(messageToDisplay) {
+  message.innerText = messageToDisplay;
+}
 
-  if(number == numberToGuess){
-    message.innerText = '🎉 Correct number'
-    document.body.style.backgroundColor = '#66dc66';
-
-    if(highScore.innerText == 0 || highScore.innerText < score){
-        highScore.innerText = score;
-    }
-  }else if(number > numberToGuess){
-    score--;
-    message.innerText = '📈 Too high!'
-    scoreEle.innerText = score;
-  }else{
-    score--;
-    message.innerText = '📉 Too low!'
-    scoreEle.innerText = score;
-
+function setHighScore() {
+  if (highScore.innerText == 0 || highScore.innerText < score) {
+    highScore.innerText = score;
   }
 }
 
-function resetGame(){
-    score = 20;
+function checkNumber() {
+  if(isLosingGame()) return;
+
+  const number = parseInt(document.getElementById("GuessingNumber").value);
+
+  if (!number) {
+    displayMessage("⛔️ NO number");
+  } else if (number == numberToGuess) {
+    displayMessage("🎉 Correct number");
+    document.body.style.backgroundColor = "#66dc66";
+    secretNumber.style.width = "300px";
+    secretNumber.innerHTML = numberToGuess;
+    setHighScore();
+  } else {
+    score--;
+    const wrongMsg = number < numberToGuess ? "📉 Too low!" : "📈 Too high!";
+    displayMessage(isLosingGame() ? "💥 You lost the game" : wrongMsg);
     scoreEle.innerText = score;
-    numberToGuess = getRandomInt();
-    document.body.style.backgroundColor = '';
-    document.getElementById("GuessingNumber").value = '';
+  }
+}
+
+function resetGame() {
+  score = 20;
+  scoreEle.innerText = score;
+  numberToGuess = getRandomInt();
+  document.body.style.backgroundColor = "";
+  document.getElementById("GuessingNumber").value = "";
+  secretNumber.style.width = "150px";
+  secretNumber.innerHTML = '?';
 }
